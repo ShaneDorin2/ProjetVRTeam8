f2afe52aa382a9f66f1030c2cff44a7288bb82e9
ShaneDorin <shane.dorin@edu.devinci.fr>
2024-12-04 09:19:45 +0100

    WIP RoadTile class + RoadTileBP

---
+++ Source/ProjetVR_Team8/Private/Road/RoadTile.cpp
@@ -0,0 +1,47 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Road/RoadTile.h"
+
+#include "MaterialHLSLTree.h"
+#include "GameFramework/PawnMovementComponent.h"
+
+
+// Sets default values
+ARoadTile::ARoadTile()
+{
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ARoadTile::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ARoadTile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	this->AddMovementInput(FVector(1, 0, 0), 10);
+	ApplyMovementInputToPawn(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ARoadTile::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+void ARoadTile::ApplyMovementInputToPawn(float DeltaTime)
+{
+	FVector AddToLocation = this->GetLastMovementInputVector() * MovementSpeed * DeltaTime;
+
+	FVector NewLocation = AddToLocation;
+	
+	this->SetActorLocation(this->GetActorLocation() + NewLocation);
+}
+
---
+++ Source/ProjetVR_Team8/Public/Road/RoadTile.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "RoadTile.generated.h"
+
+UCLASS()
+class PROJETVR_TEAM8_API ARoadTile : public APawn
+{
+	GENERATED_BODY()
+#pragma region Unreal Default
+	
+public:
+	// Sets default values for this pawn's properties
+	ARoadTile();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+#pragma endregion
+	
+private:
+	void ApplyMovementInputToPawn(float DeltaTime);
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float MovementSpeed = 10;
+};
